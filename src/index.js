557f5e11ee09e9c921bf3b1b6cce8909b0158d82
synarx <137931980+scandaloux@users.noreply.github.com>
2024-07-17 06:58:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import { fork } from 'child_process'
-import path from 'path'
+import { fork } from 'child_process';
+import path from 'node:path';
+
+const imageFork = path.join(import.meta.dirname, 'imageFork.js');
 
 export function processImages (bulk) {
     return new Promise((resolve, reject) => {
         const returns = [];
     
-        const imageProcessor = fork(path.join(import.meta.dirname, 'process_fork_image.mjs'));
-        imageProcessor.send(bulk)
+        const imageProcessor = fork(imageFork);
+        imageProcessor.send(bulk);
     
         imageProcessor.on('message', (data) => {
-            const i = data.shift()
+            const i = data.shift();
             returns[i] = data;
             if (returns.filter(Boolean).length === bulk.data.length) {
-                imageProcessor.kill()
+                imageProcessor.kill();
                 resolve(returns);
             }
         });
@@ -21,8 +23,8 @@ export function processImages (bulk) {
 
 export default async function (fastify) {
     fastify.post('/process-images', async (req, rep) => {
-        const result = await processImages(req.body)
-        console.log(result)
-        rep.send(result)
+        const result = await processImages(req.body);
+        console.log(result);
+        rep.send(result);
     })
 }
